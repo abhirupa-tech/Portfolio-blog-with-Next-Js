953fec4ea7d54f8679f78173267aa84bbf6210a8
Abhirupa Mitra <mitra.abhirupa@gmail.com>
2024-05-01 10:28:57 +0530

    Updates

---
+++ precache-manifest.31ae69eabb4ad5e0c2d5d0cb9eb70735.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "179b6ec0c44def26dc69ab37e284970d",
+    "url": "/Portfolio-blog-with-Next-Js/index.html"
+  },
+  {
+    "revision": "1d618cfd6fef8a6cb715",
+    "url": "/Portfolio-blog-with-Next-Js/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "30d245f0f8261366b710",
+    "url": "/Portfolio-blog-with-Next-Js/static/js/2.78aebd54.chunk.js"
+  },
+  {
+    "revision": "89f1b586a975439d2fb1",
+    "url": "/Portfolio-blog-with-Next-Js/static/js/3.34e9bfbd.chunk.js"
+  },
+  {
+    "revision": "1d618cfd6fef8a6cb715",
+    "url": "/Portfolio-blog-with-Next-Js/static/js/main.b90f29b4.chunk.js"
+  },
+  {
+    "revision": "2ad2197be5ab4d841b63",
+    "url": "/Portfolio-blog-with-Next-Js/static/js/runtime~main.89cc7c9a.js"
+  },
+  {
+    "revision": "738419c3ec95241a3d56e4654555005e",
+    "url": "/Portfolio-blog-with-Next-Js/static/media/AbrilFatface-Regular.738419c3.ttf"
+  },
+  {
+    "revision": "1edb1a244f44d0884a125df9c2a11577",
+    "url": "/Portfolio-blog-with-Next-Js/static/media/HomePageBackground.1edb1a24.png"
+  },
+  {
+    "revision": "cce7ff8c1d7999f907b6760fbe75d99d",
+    "url": "/Portfolio-blog-with-Next-Js/static/media/Montserrat-Black.cce7ff8c.ttf"
+  },
+  {
+    "revision": "ed86af2ed5bbaf879e9f2ec2e2eac929",
+    "url": "/Portfolio-blog-with-Next-Js/static/media/Montserrat-Bold.ed86af2e.ttf"
+  },
+  {
+    "revision": "a7fe50578d9aa3966c925cb9722db03a",
+    "url": "/Portfolio-blog-with-Next-Js/static/media/Montserrat-ExtraLight.a7fe5057.ttf"
+  },
+  {
+    "revision": "36bcc537dc03505d47b5c6ada975cb9a",
+    "url": "/Portfolio-blog-with-Next-Js/static/media/MontserratAlternates-Light.36bcc537.ttf"
+  },
+  {
+    "revision": "45549d1050904333d8d67ee7edc3fdee",
+    "url": "/Portfolio-blog-with-Next-Js/static/media/ProfilePicture.45549d10.png"
+  },
+  {
+    "revision": "e7e73bae4d79b8f78a2fe71816050204",
+    "url": "/Portfolio-blog-with-Next-Js/static/media/blog1img.e7e73bae.jpg"
+  },
+  {
+    "revision": "9f8b0cba06df0c52238fd712f5d08405",
+    "url": "/Portfolio-blog-with-Next-Js/static/media/blog3img.9f8b0cba.jpg"
+  },
+  {
+    "revision": "de959396ff7c1302a0bc59172d215a3f",
+    "url": "/Portfolio-blog-with-Next-Js/static/media/emailIcon.de959396.png"
+  },
+  {
+    "revision": "3809eca89c3a08d061c65e7226c057a6",
+    "url": "/Portfolio-blog-with-Next-Js/static/media/githubIcon.3809eca8.png"
+  },
+  {
+    "revision": "8df7003f367726cd246f5dbc2ebb4557",
+    "url": "/Portfolio-blog-with-Next-Js/static/media/instagramIcon.8df7003f.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b289b31e6bc9930294da00a5ca05cd25.js"
+  "/Portfolio-blog-with-Next-Js/precache-manifest.31ae69eabb4ad5e0c2d5d0cb9eb70735.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Portfolio-blog-with-Next-Js/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
