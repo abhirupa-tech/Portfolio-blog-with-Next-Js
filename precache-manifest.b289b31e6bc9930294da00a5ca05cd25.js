0a842c27ce5dc4402d3b192506e8b7dfec56a365
Abhirupa Mitra <mitra.abhirupa@gmail.com>
2024-05-01 10:15:13 +0530

    Updates

---
+++ precache-manifest.b289b31e6bc9930294da00a5ca05cd25.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d404441864f2927cddc64d654e93844a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1d618cfd6fef8a6cb715",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "2755c7ca6ff1a006eaf9",
+    "url": "/static/js/2.e3590cc8.chunk.js"
+  },
+  {
+    "revision": "89f1b586a975439d2fb1",
+    "url": "/static/js/3.34e9bfbd.chunk.js"
+  },
+  {
+    "revision": "1d618cfd6fef8a6cb715",
+    "url": "/static/js/main.b90f29b4.chunk.js"
+  },
+  {
+    "revision": "1f15307e7b638c110544",
+    "url": "/static/js/runtime~main.1677de94.js"
+  },
+  {
+    "revision": "738419c3ec95241a3d56e4654555005e",
+    "url": "/static/media/AbrilFatface-Regular.738419c3.ttf"
+  },
+  {
+    "revision": "1edb1a244f44d0884a125df9c2a11577",
+    "url": "/static/media/HomePageBackground.1edb1a24.png"
+  },
+  {
+    "revision": "cce7ff8c1d7999f907b6760fbe75d99d",
+    "url": "/static/media/Montserrat-Black.cce7ff8c.ttf"
+  },
+  {
+    "revision": "ed86af2ed5bbaf879e9f2ec2e2eac929",
+    "url": "/static/media/Montserrat-Bold.ed86af2e.ttf"
+  },
+  {
+    "revision": "a7fe50578d9aa3966c925cb9722db03a",
+    "url": "/static/media/Montserrat-ExtraLight.a7fe5057.ttf"
+  },
+  {
+    "revision": "36bcc537dc03505d47b5c6ada975cb9a",
+    "url": "/static/media/MontserratAlternates-Light.36bcc537.ttf"
+  },
+  {
+    "revision": "45549d1050904333d8d67ee7edc3fdee",
+    "url": "/static/media/ProfilePicture.45549d10.png"
+  },
+  {
+    "revision": "e7e73bae4d79b8f78a2fe71816050204",
+    "url": "/static/media/blog1img.e7e73bae.jpg"
+  },
+  {
+    "revision": "9f8b0cba06df0c52238fd712f5d08405",
+    "url": "/static/media/blog3img.9f8b0cba.jpg"
+  },
+  {
+    "revision": "de959396ff7c1302a0bc59172d215a3f",
+    "url": "/static/media/emailIcon.de959396.png"
+  },
+  {
+    "revision": "3809eca89c3a08d061c65e7226c057a6",
+    "url": "/static/media/githubIcon.3809eca8.png"
+  },
+  {
+    "revision": "8df7003f367726cd246f5dbc2ebb4557",
+    "url": "/static/media/instagramIcon.8df7003f.png"
+  }
+]);
